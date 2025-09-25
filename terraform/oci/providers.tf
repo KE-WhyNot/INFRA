aea1303a21318ab17d540e6927c6677932e194c2
dayoung <dayoungann@naver.com>
2025-09-25 14:35:55 +0900

    [FEAT] oci helm, argo 추가

--- terraform/oci/providers.tf
+++ terraform/oci/providers.tf
@@ -3,15 +3,40 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "~> 5.30"
+      version = "~> 7.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.17"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.38"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4"
     }
   }
 }
 
+# OCI provider
 provider "oci" {
-  region           = "ap-chuncheon-1"
+  region           = var.region
   tenancy_ocid     = var.tenancy_id
   user_ocid        = var.user_id
   fingerprint      = var.fingerprint
   private_key_path = var.private_key_path
+}
+
+# Kubernetes provider 
+provider "kubernetes" {
+  config_path = pathexpand("~/.kube/oke.yaml")
+}
+
+# Helm provider
+provider "helm" {
+  kubernetes {
+    config_path = pathexpand("~/.kube/oke.yaml")
+  }
 }
